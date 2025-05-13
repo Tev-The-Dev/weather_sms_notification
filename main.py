d784f4d30e8bd3e1464befa710760bebaed3c1bc
Tevyn <tevyn.payne@gmail.com>
2025-05-13 00:13:21 -0500

    connecting to sms services and securing my env variables

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 import requests
 
+from twilio.rest import Client
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+
+
 api_call = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "0853df0fe250da1fe3dc2fbf53c9ced7"
+api_key = os.getenv("WEATHER_API_KEY")
+
+my_phone = os.getenv("MY_PHONE")
+send_phone = os.getenv("WHATSAPP_PHONE")
+
 long = -97.082685
 lat = 32.837287
 parameters = {
@@ -24,7 +37,10 @@ for entry in data["list"]: #loops through every item in the list
         will_rain=True
     print(weather_info["id"],"-",weather_info["description"]) #prints the id and description of the weather item
 #print(data["list"][0]["weather"][0]["id"], data["list"][0]["weather"][0]["description"])
+client = Client(account_sid, auth_token)
 if will_rain:
-    print("bring an umbrella")
+    message = client.messages.create(from_=send_phone,body='Bring and umbrella. Chance of rain high.',to=my_phone)
+    print(message.status)
 else:
-    print("No rain in sight for the next 12 hours")
+    message = client.messages.create(from_=send_phone,body= 'No need to bring an umbrella. Chance of rain low',to = my_phone)
+    print(message.status)
